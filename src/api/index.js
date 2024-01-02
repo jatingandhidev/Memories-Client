b4b28a36fb448f3e44a82d6adb23863603d96f08
Salvad0r14 <jatin05092001@gmail.com>
2024-01-02 20:02:36 +0530

    commit redirects

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: 'https://mern-memories-server-wqsz.onrender.com',
+})
 
 //adding this to each req
 API.interceptors.request.use((req) => {
